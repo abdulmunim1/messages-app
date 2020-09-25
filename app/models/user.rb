b700aab36eab5c5e262555a9998f4b64bff34990
Abdul Munim <farzimunim@gmail.com>
2020-09-25 08:45:05 -0700

    Added login with name

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,10 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   validates_presence_of :name, :email, :password
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable
 
   has_many :messages
-
+  validates :name, uniqueness: true
+  validates :email, format: { with: /\A[^@\s]+@[^@\s]+\z/ , message: "Email invalid"  }
+  validates :password, length: { minimum:6 }
   scope :valid_receivers, -> { where.not(token: nil)}
 
   def self.get_registration_tokens_except user
