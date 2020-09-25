76d41489c05a37200ecc2e571393ea42bd73092c
Abdul Munim <farzimunim@gmail.com>
2020-09-25 11:06:15 -0700

    fixed minor issue

--- db/schema.rb
+++ db/schema.rb
@@ -16,7 +16,6 @@ ActiveRecord::Schema.define(version: 2020_09_24_092301) do
   enable_extension "plpgsql"
 
   create_table "messages", force: :cascade do |t|
-    t.string "title"
     t.text "message"
     t.bigint "user_id"
     t.datetime "created_at", precision: 6, null: false
@@ -27,13 +26,15 @@ ActiveRecord::Schema.define(version: 2020_09_24_092301) do
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
+    t.string "username", null: false
+    t.string "name", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "token"
-    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["name"], name: "index_users_on_name", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
