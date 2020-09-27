eaeea9cb7d88a151ee27e7147ed4589135a7c931
Abdul Munim <farzimunim@gmail.com>
2020-09-27 14:30:20 -0700

    scroll to bottom after reload

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,16 +3,20 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-require("@rails/ujs").start()
-require("turbolinks").start()
-require("@rails/activestorage").start()
-require("channels")
-import "bootstrap"
+require("@rails/ujs").start();
+require("turbolinks").start();
+require("@rails/activestorage").start();
+require("channels");
+import "bootstrap";
 
-$(function(){
-    var elem = document.getElementById("messages")
-    elem.scrollTo(0, elem.scrollHeight)
-})
+$(function () {
+  var elem = document.getElementById("messages");
+  elem.scrollTo(0, elem.scrollHeight);
+  document.addEventListener("turbolinks:load", () => {
+    var elem = document.getElementById("messages");
+    elem.scrollTo(0, elem.scrollHeight);
+  });
+});
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
