e700d9559e09ab984aaaddd2807f5f5a8b2eb86c
Abdul Munim <farzimunim@gmail.com>
2020-09-28 07:25:32 -0700

    added scroll down on text input focus

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -10,12 +10,16 @@ require("channels");
 import "bootstrap";
 
 $(function () {
-  var elem = document.getElementById("messages");
-  elem.scrollTo(0, elem.scrollHeight);
-  document.addEventListener("turbolinks:load", () => {
-    var elem = document.getElementById("messages");
-    elem.scrollTo(0, elem.scrollHeight);
+    function scrollDown()
+    {
+        var elem = document.getElementById("messages");
+        elem.scrollTo(0, elem.scrollHeight);    
+    }
+    scrollDown();
+    document.addEventListener("turbolinks:load", () => {
+        scrollDown();
   });
+  $('#send-message-text').on('focusin',()=>{scrollDown()})
 });
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
