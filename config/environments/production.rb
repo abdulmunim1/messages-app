7de9a0fd2e2debf9b55143ed96fb5703a35d5315
Abdul Munim <farzimunim@gmail.com>
2020-09-27 22:37:58 -0700

    white list actioncable url

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,6 +3,8 @@ Rails.application.configure do
 
   # Code is not reloaded between requests.
   config.cache_classes = true
+  config.action_cable.url = 'https://morning-badlands-40082.herokuapp.com/cable'
+  config.action_cable.allowed_request_origins = [ 'https://morning-badlands-40082.herokuapp.com/']
 
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
